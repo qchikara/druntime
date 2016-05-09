ac229154a5414e15b4c7faa54bdf765a33302ead
qchikara <jiki@red.email.ne.jp>
2016-05-09 19:49:26 +0900

    Add struct member rpcdcep
    undocumented and little use

--- src/core/sys/windows/rpcdcep.d
+++ src/core/sys/windows/rpcdcep.d
@@ -82,6 +82,7 @@ struct RPC_SERVER_INTERFACE {
     PRPC_PROTSEQ_ENDPOINT RpcProtseqEndpoint;
     void*                 DefaultManagerEpv;
 const(void)*          InterpreterInfo;
+    uint                  Flags;
 }
 alias RPC_SERVER_INTERFACE* PRPC_SERVER_INTERFACE;
 
@@ -94,6 +95,7 @@ struct RPC_CLIENT_INTERFACE {
     PRPC_PROTSEQ_ENDPOINT RpcProtseqEndpoint;
     ULONG_PTR             Reserved;
 const(void)*          InterpreterInfo;
+    uint                  Flags;
 }
 alias RPC_CLIENT_INTERFACE* PRPC_CLIENT_INTERFACE;
 
